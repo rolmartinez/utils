6c17de0bd7d04d4b5a4b69c9eb30ceb553a2d3be
rolmartinez <roland.martinez@trustradius.com>
2024-04-24 12:00:33 -0500

    update to delete statement

--- bigquery/delete_ga4_invalid_data.sql
+++ bigquery/delete_ga4_invalid_data.sql
@@ -14,16 +14,35 @@ SET tables_to_delete = (
 -- Step 2: Check the Values in tables_to_delete
 SELECT * FROM UNNEST(tables_to_delete);
 
--- Step 3: Delete Rows Based on Conditions
-FOR table_name IN (SELECT * FROM UNNEST(tables_to_delete) tbl_name) DO
+
+-- Step 3: Execute DELETE Statements with JOIN
+FOR table_name IN (SELECT * FROM UNNEST(tables_to_delete) AS tbl_name) DO
   EXECUTE IMMEDIATE FORMAT(
     """
     DELETE FROM `analytics_314488492.%s`
-    WHERE user_pseudo_id IN (
-      SELECT user_pseudo_id
-      FROM `tr-ga4.analytics_314488492.session_deletion_candidates`
-    )
+    WHERE
+      user_pseudo_id IN (
+        SELECT candidates_user.user_pseudo_id
+        FROM
+          `tr-ga4.analytics_314488492.session_deletion_candidates` AS candidates_user
+      )
+      AND
+        (SELECT
+          MAX(value.int_value)
+        FROM
+          UNNEST(`analytics_314488492.%s`.event_params)
+        WHERE
+          key = 'ga_session_id'
+          LIMIT 1
+      ) = (
+        SELECT
+          candidates_ga.ga_session_id
+        FROM
+          `tr-ga4.analytics_314488492.session_deletion_candidates` AS candidates_ga
+        LIMIT 1  -- Ensure single result
+      )
     """,
+    table_name.tbl_name,
     table_name.tbl_name
   );
 END FOR;
